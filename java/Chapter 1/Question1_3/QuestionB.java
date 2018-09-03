32d46254b513c37a0f00eaabaec9c89215daa8b2
rahuls0720 <rahuls0720@gmail.com>
2018-09-03 08:13:00 -0600

    solution to ch 1, question 1 and 3 complete; ctci solutions are commented out

--- java/Chapter 1/Question1_3/QuestionB.java
+++ java/Chapter 1/Question1_3/QuestionB.java
@@ -1,25 +1,45 @@
 package Question1_3;
 
 public class QuestionB {	
-	public static boolean permutation(String s, String t) {
-		if (s.length() != t.length()) {
+	// public static boolean permutation(String s, String t) {
+	// 	if (s.length() != t.length()) {
+	// 		return false;
+	// 	}
+		
+	// 	int[] letters = new int[128];
+		 
+	// 	char[] s_array = s.toCharArray();
+	// 	for (char c : s_array) { // count number of each char in s.
+	// 		letters[c]++;  
+	// 	}
+		  
+	// 	for (int i = 0; i < t.length(); i++) {
+	// 		int c = (int) t.charAt(i);
+	// 	    if (--letters[c] < 0) {
+	// 	    	return false;
+	// 	    }
+	// 	}
+		  
+	// 	return true;
+	// }
+
+	// st2 needs to be formed rearranging chars in str1; str1 len == str2 len
+	public static boolean permutation(String str1, String str2) {
+		if (str1.length() != str2.length()) {
 			return false;
 		}
-		
-		int[] letters = new int[128];
-		 
-		char[] s_array = s.toCharArray();
-		for (char c : s_array) { // count number of each char in s.
-			letters[c]++;  
+
+		int[] letter_count = new int[26];
+		for (int i = 0; i < str1.length(); i++) { 
+			letter_count[str1.charAt(i) - 'a']++;
 		}
-		  
-		for (int i = 0; i < t.length(); i++) {
-			int c = (int) t.charAt(i);
-		    if (--letters[c] < 0) {
-		    	return false;
-		    }
+
+		for (int i = 0; i < str2.length(); i++) {
+			if (--letter_count[str2.charAt(i) - 'a'] < 0) {
+				return false;
+			} 
 		}
-		  
+
 		return true;
 	}
 	
